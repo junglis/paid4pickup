4ea32f77bd9c1df9a314063ca73b7128d8fcf3a6
Nathan Land <nland@dyn.com>
2015-06-08 12:05:14 -0700

    screwing around with tableviews

--- paid4pickup/ViewController.swift
+++ paid4pickup/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+        // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+        return 9
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
